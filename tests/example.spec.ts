dabd80e260447fcb3875b1c6c2b5f6be1e499ba9
Felix <phuclm96@gmail.com>
2024-12-26 15:44:29 +0700

    exercise_1

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,19 @@
 import { test, expect } from '@playwright/test';
+const URL = 'https://material.playwrightvn.com/'
 
 test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto(URL);
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(page).toHaveTitle('Tài liệu học automation test - Playwright Việt Nam');
+  expect(await page.title()).toContain('Tài liệu học automation test');
 });
 
 test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  await page.goto(URL);
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
+  await page.getByRole('link', { name: 'Bài học 1: Register Page' }).click();
   // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'User Registration' })).toBeVisible();
 });
