3c9e961714245c63cbb288c1ad394bb4d5a17995
Udemy - Phuc <phuclm96@gmail.com>
2025-03-22 16:10:18 +0700

    refactor code

--- tests/lession-5/test2.spec.ts
+++ tests/lession-5/test2.spec.ts
@@ -1,4 +1,6 @@
 import test, { expect } from "@playwright/test";
+
+const URL = "https://material.playwrightvn.com/";
 const PRODUCTS = [
   { PRODUCT_NAME: "Product 1", PRODUCT_PRICE: 10, PRODUCT_QUANTITY: 5 },
   { PRODUCT_NAME: "Product 2", PRODUCT_PRICE: 20, PRODUCT_QUANTITY: 3 },
@@ -8,46 +10,49 @@ const PRODUCTS = [
 test.describe("Exercise add product in Product page", () => {
   test("Product page", async ({ page }) => {
     await test.step("Go to product page", async () => {
-      await page.goto("https://material.playwrightvn.com/");
+      await page.goto(URL);
       await page.locator("//a[contains(text(),'Product page')]").click();
-      await page.waitForResponse(
-        "https://material.playwrightvn.com/product-03.jpg"
-      );
     });
 
     await test.step("Add product", async () => {
-      for (const PRODUCT of PRODUCTS) {
-        await page
-          .locator(
-            `//div[text()='${PRODUCT.PRODUCT_NAME}']/following-sibling::button`
-          )
-          .click({ clickCount: PRODUCT.PRODUCT_QUANTITY, delay: 100 });
+      for (const product of PRODUCTS) {
+        try {
+          const addButton = page.locator(
+            `//div[text()='${product.PRODUCT_NAME}']/following-sibling::button`
+          );
+          await addButton.waitFor({ state: "visible" });
+          await addButton.click({
+            clickCount: product.PRODUCT_QUANTITY,
+            delay: 100,
+          });
+        } catch (error) {
+          console.error(`Failed to add product for ${product.PRODUCT_NAME}`);
+          throw error;
+        }
       }
     });
 
-    let total: number;
-    let grandTotal: number = 0;
+    await test.step("Verify the result table", () => {
+      let grandTotal: number = 0;
 
-    PRODUCTS.forEach((PRODUCT) => {
-      total = PRODUCT.PRODUCT_QUANTITY * PRODUCT.PRODUCT_PRICE;
+      PRODUCTS.forEach((product) => {
+        const subTotal = product.PRODUCT_QUANTITY * product.PRODUCT_PRICE;
+        grandTotal += subTotal;
 
-      expect(
-        page.locator(
-          `//td[text()='${PRODUCT.PRODUCT_NAME}']/following-sibling::td[2]`
-        )
-      ).toHaveText(PRODUCT.PRODUCT_QUANTITY.toString());
+        const quantityCell = page.locator(
+          `//td[text()='${product.PRODUCT_NAME}']/following-sibling::td[2]`
+        );
+        const productNameCell = page.locator(
+          `//td[text()='${product.PRODUCT_NAME}']/following-sibling::td[3]`
+        );
 
-      expect(
-        page.locator(
-          `//td[text()='${PRODUCT.PRODUCT_NAME}']/following-sibling::td[3]`
-        )
-      ).toContainText(total.toString());
+        expect(quantityCell).toHaveText(product.PRODUCT_QUANTITY.toString());
+        expect(productNameCell).toContainText(subTotal.toString());
+      });
 
-      grandTotal += total;
+      expect(page.locator('//td[@class="total-price"]')).toContainText(
+        grandTotal.toString()
+      );
     });
-
-    expect(page.locator('//td[@class="total-price"]')).toContainText(
-      grandTotal.toString()
-    );
   });
 });
